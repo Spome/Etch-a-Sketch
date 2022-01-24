86fedb3fe7ad1717c6c703cd98d9e0d48b09661d
Spome <zaposar@gmail.com>
2022-01-24 15:45:08 +0200

    Changed to use CSS grid instead of flexbox

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
+
 let containerDiv = document.querySelector("#container")
 
 //containerDiv.style.backgroundColor = "red";
 
 let clearButton = document.querySelector("#clearButton");
-clearButton.addEventListener('click', () => {
 
-});
+let size = 64;
 
-for (let i=1; i<=16; i++) {
-  for (let j=1; j<=16; j++) {
+for (let i=1; i <= size; i++) {
+  for (let j=1; j <= size; j++) {
     const gridDiv = document.createElement('div')
-    gridDiv.style.border = "1px solid black";
-    gridDiv.style.backgroundColor = "blue";
-    gridDiv.textContent = "test"
-    gridDiv.classList.add("gridDiv")
+    gridDiv.style.backgroundColor = "white";
+    gridDiv.classList.add("gridDiv");
     gridDiv.addEventListener("mouseover", function(event) {
-      event.target.style.backgroundColor = "orange";
+      event.target.style.backgroundColor = "black";
     });
 
     containerDiv.appendChild(gridDiv);
   }
 }
+
+containerDiv.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
+function clearAll() {
+  document.querySelectorAll('.gridDiv').forEach(function(node) {
+    node.style.backgroundColor = "white";
+  })
+}
+
+clearButton.addEventListener('click', clearAll);
+
 
